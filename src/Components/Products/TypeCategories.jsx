24120fd43fb6a38a1788980d12c18c130dbf4b08
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-03-18 12:42:17 -0500

    added new font styling

--- src/Components/Products/TypeCategories.jsx
+++ src/Components/Products/TypeCategories.jsx
@@ -23,8 +23,8 @@ export default function TypeCategories() {
     <>
       <NavbarUser />
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">{type} Categories</h2>
+        <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-semibold tracking-tight text-gray-900">{type} Categories</h2>
           <div className="py-10 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {categories.map((category) => (
               <>
