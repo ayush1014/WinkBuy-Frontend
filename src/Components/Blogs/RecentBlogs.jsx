f9710bd2a539e385d4b25561c6281daf46527da1
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-03-18 22:33:22 -0500

    fixed styling

--- src/Components/Blogs/RecentBlogs.jsx
+++ src/Components/Blogs/RecentBlogs.jsx
@@ -27,7 +27,7 @@ export default function RecentBlogs() {
     return (
         <>
             <div className="bg-white py- sm:py-1">
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-7xl px-4 lg:px-8">
                     <h2 className="text-sm md:text-xl lg:text-2xl font-normal tracking-tight text-gray-700">Recent Drops on Wink Blogs</h2>
                     <div className="mx-auto mt-2 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         {posts.map((post) => (
