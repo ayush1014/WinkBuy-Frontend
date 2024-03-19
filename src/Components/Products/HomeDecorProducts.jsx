d6eb9074ed49e001c514814adda7f06f7bf7cdbe
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-03-19 12:39:32 -0500

    added new style in product category

--- src/Components/Products/HomeDecorProducts.jsx
+++ src/Components/Products/HomeDecorProducts.jsx
@@ -120,7 +120,7 @@ export default function HomeDecorProducts() {
     <div>
       <NavbarUser />
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl px-4 py-1 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-semibold tracking-tight text-gray-900">Recent Drops</h2>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
